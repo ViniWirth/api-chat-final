ada216ca18d761b2162337c504b7708bc25214d7
Vinicius Wirth <viniwirth2006@gmail.com>
2024-08-27 20:54:01 -0300

    commit

--- src/api.js
+++ src/api.js
@@ -26,12 +26,15 @@ app.use('/', router.get('/sobre', (req, res, next) => {
     res.status(200).send(resp);
 }));
 */
+
+//entrar conta
 app.use("/entrar", router.post("/entrar", async(req, res, next)=>{
     const usuarioController = require("./controllers/usuarioController.js");
     let resp = await usuarioController.entrar(req.body.nick);
     res.status(200).send(resp);
 }));
 
+//listar salas
 app.use("/salas",router.get("/salas", async (req, res,next) => {
     const token = require("./util/token");
     const salaController = require("./controllers/salaController");
@@ -45,6 +48,7 @@ app.use("/salas",router.get("/salas", async (req, res,next) => {
     }
 })) 
 
+//entrar na sala
 app.use("/sala/entrar", router.post("/sala/entrar", async (req, res)=>{
     const token = require("./util/token");
     const salaController = require("./controllers/salaController");
@@ -55,16 +59,19 @@ app.use("/sala/entrar", router.post("/sala/entrar", async (req, res)=>{
         res.status(401).send({msg:"Usuário não autorizado"});
     }
 }));
-  
-app.use("/sala/enviar", router.post("/sala/enviar", async (req, res) => {
+
+//enviar mensagem
+app.use("/sala/mensagem", router.post("/sala/mensagem", async (req, res) => {
     const token = require("./util/token");
     const salaController = require("./controllers/salaController");
     if(!token.checkToken(req.headers.token,req.headers.iduser,req.headers.nick)) return false;
-    let resp= await salaController.enviarMensagem(req.headers.nick, req.body.msg,req.body.idSala);
+    let resp= await salaController.enviarMensagem(req.headers.nick, req.body.mensagem,req.body.idSala);
     res.status(200).send(resp);
 }))
   
-app.use("/sala/listar", router.get("/sala/listar", async (req, res) => {
+
+//lista mensagens
+app.use("/sala/mensagens", router.get("/sala/mensagens", async (req, res) => {
     const token = require("./util/token");
     const salaController = require("./controllers/salaController");
     if(!token.checkToken(req.headers.token,req.headers.iduser,req.headers.nick)) return false;
